c09edc03e6a29f12f3a45c8e3ef77edbf79f6131
MobileApplicationDev <sasikala@smartinfraservice.com>
2021-02-24 14:47:27 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController  } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  taskList = [];
+  taskName: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController) {
 
   }
+  addTask() {
+    if (this.taskName.length > 0) {
+        let task = this.taskName;
+        this.taskList.push(task);
+        this.taskName = "";
+    }
+}
+
+deleteTask(index){
+  this.taskList.splice(index, 1);
+}
+updateTask(index) {
+  let alert = this.alertCtrl.create({
+      title: 'Update Task?',
+      message: 'Type in your new task to update.',
+      inputs: [{ name: 'editTask', placeholder: 'Task' }],
+      buttons: [{ text: 'Cancel', role: 'cancel' },
+                { text: 'Update', handler: data => {
+                    this.taskList[index] = data.editTask; }
+                }
+               ]
+  });
+  alert.present();
+}
 
 }
