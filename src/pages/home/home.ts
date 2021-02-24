e707da26fc4840dc94ad8adb14e4ba73906eceb5
MobileApplicationDev <sasikala@smartinfraservice.com>
2021-02-24 16:06:24 +0530

    second commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,41 +1,50 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController  } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+
+  
   taskList = [];
   taskName: any;
 
   constructor(public navCtrl: NavController,
-              public alertCtrl: AlertController) {
+    public alertCtrl: AlertController) {
 
   }
+
   addTask() {
     if (this.taskName.length > 0) {
-        let task = this.taskName;
-        this.taskList.push(task);
-        this.taskName = "";
+      let task = this.taskName;
+      console.log(task);
+      this.taskList.push(task);
+      console.log(this.taskList);
+      this.taskName = "";
     }
-}
+  }
 
-deleteTask(index){
-  this.taskList.splice(index, 1);
-}
-updateTask(index) {
-  let alert = this.alertCtrl.create({
+  deleteTask(index) {
+    this.taskList.splice(index, 1);
+  }
+
+  updateTask(index) {
+    let alert = this.alertCtrl.create({
       title: 'Update Task?',
       message: 'Type in your new task to update.',
       inputs: [{ name: 'editTask', placeholder: 'Task' }],
       buttons: [{ text: 'Cancel', role: 'cancel' },
-                { text: 'Update', handler: data => {
-                    this.taskList[index] = data.editTask; }
-                }
-               ]
-  });
-  alert.present();
-}
+      {
+        text: 'Update', handler: data => {
+          this.taskList[index] = data.editTask;
+        }
+      }
+      ]
+    });
+    alert.present();
+  }
 
 }
